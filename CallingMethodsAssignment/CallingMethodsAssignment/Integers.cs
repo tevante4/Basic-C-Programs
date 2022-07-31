1be179541d1a390d4c0746643fd5bdff79b45139
Tevante Swanston <tevswany@gmail.com>
2022-07-31 00:35:33 -0700

    Calling Methods

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Integers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    public class Integers
+    {
+        public int Add(int add)
+        {
+
+            return add + 10;
+        }
+        public int Subtract(int sub)
+        {
+
+            return sub - 5;
+        }
+        public int Multiply(int mult)
+        {
+
+            return mult * 3;
+        }
+
+    }
+}
---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Integers add = new Integers();
+
+            Console.WriteLine("Input a number to add 10 to it");
+            int yourNum = Convert.ToInt32(Console.ReadLine());
+            int result = add.Add(yourNum); //instance of Integers called add, passed in number 
+            Console.WriteLine(result);
+
+
+            Console.WriteLine("Input a number to subtract 5 from it");
+            int subNum = Convert.ToInt32(Console.ReadLine());
+            result = add.Subtract(subNum);
+            Console.WriteLine(result);
+
+
+            Console.WriteLine("Input a number to be multiplied by 3");
+            int multNum = Convert.ToInt32(Console.ReadLine());
+            result = add.Multiply(multNum);
+            Console.WriteLine(result);
+
+
+
+
+
+
+
+            Console.Read();
+
+        }
+    }
+}

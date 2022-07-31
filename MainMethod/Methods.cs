814856042a2a1cd5be8e3ae0c1d802b0e1a5aad8
Tevante Swanston <tevswany@gmail.com>
2022-07-31 00:55:22 -0700

    main method assignment

---
+++ MainMethod/Methods.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    public class Methods
+    {
+        // --- Original Work below, revision follows --
+        //public int Add(int a, int b)
+        //{
+        //    return a + b;
+        //}
+
+        //public int Add(decimal c, decimal d)
+        //{
+        //    int f = Convert.ToInt32(c * d);
+        //    return f;
+        //}
+
+
+        //public int Add(string g, string h)
+        //{
+        //    int i = Convert.ToInt32(g);
+        //    int j = Convert.ToInt32(h);
+        //    int k = i - j;
+        //    return k;    
+        //}
+
+
+        //Revision work, methods of the same name to demonstrate overloaded methods 
+        public int Add(int a)
+        {
+            return a + 4;
+        }
+
+        public int Add(decimal c)
+        {
+            int f = Convert.ToInt32(c * 4);
+            return f;
+        }
+
+        public int Add(string g)
+        {
+            int i = Convert.ToInt32(g);
+            int k = i - 4;
+            return k;
+        }
+
+
+
+
+    }
+}
---
+++ MainMethod/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calls from Methods class to use here
+            Methods methods = new Methods();
+
+           
+            //Console.WriteLine("Method Overloading. \nAll using Add method, let's utilize integers. First number please.");
+            //int numA = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("And the second integer please.");
+            //int numB = Convert.ToInt32(Console.ReadLine());
+            //int numC = methods.Add(numA, numB);
+
+
+            //Console.WriteLine("Adding them together is: " + numC + "\nNow two decimals multiplied, returned as an integer. First number with a decimal please.");
+            //decimal numD = decimal.Parse(Console.ReadLine());
+            //Console.WriteLine("And the second number with decimal please.");
+            //decimal numE = decimal.Parse(Console.ReadLine());
+
+            //int numF = methods.Add(numD, numE);
+            //Console.WriteLine("Results: " + numF + ". \nNow to a method to convert string to integer and subract, if possible. \nFirst number please.");
+
+            //string numG = Console.ReadLine();
+            //Console.WriteLine("And number four:");
+            //string numH = Console.ReadLine();
+
+            //int numI = methods.Add(numG, numH);
+            //Console.WriteLine("Results: " + numI);
+
+
+            //Revised work - decalred variables rather than user input
+            int a = 5;
+            decimal b = 5.6M;
+            string c = "5";
+
+            //Calls Add method, demonstrates overloads for decimal and string
+            int d = methods.Add(a);
+            Console.WriteLine("Integer: 5 + 4 is: " + d);
+
+            int e = methods.Add(b);
+            Console.WriteLine("Decimal: 5.6 * 4 converted to an integer: " + e);
+
+            int f = methods.Add(c);
+            Console.WriteLine("String: 5 - 4: " + f);
+
+
+
+
+
+            Console.ReadLine();    
+        }
+    }
+}

3a8d03cac7acd0e3393e6535f0ad349a486b1598
Tevante Swanston <tevswany@gmail.com>
2022-08-03 12:51:12 -0700

    Method Assignment280

---
+++ MethodSubmission2/MethodSubmission2.cs/ClassOne.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission2.cs
+{
+    public class ClassOne
+    {
+        public int ExampleOne(int num1, int num2=5)
+        {
+            {
+                int sum1 = num1 * num2;
+                return sum1;
+            }            
+        }
+    }
+}
---
+++ MethodSubmission2/MethodSubmission2.cs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission2.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassOne math = new ClassOne();
+
+            Console.WriteLine("Please enter two numbers one at a time. A second number is not necessary.");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                int userNumber2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(math.ExampleOne(userNumber, userNumber2));
+                Console.ReadLine();
+            }
+            catch //passing the catch statement like an else statement. 
+            {
+                Console.WriteLine(math.ExampleOne(userNumber)); //this will use the user input for userNumber and the default value from the method for userNumber2
+                Console.ReadLine();
+            }
+        }
+    }
+}

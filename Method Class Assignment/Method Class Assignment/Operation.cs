f9e88678aed5178346ab8d8bb10efb741e970b80
Tevante Swanston <tevswany@gmail.com>
2022-08-07 09:50:15 -0700

    Method Class Submission

---
+++ Method Class Assignment/Method Class Assignment/Operation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    public class Operation
+    {
+        public void Multi(int mult, int mult1)
+        {
+            int answer = mult + 20;
+            Console.WriteLine(mult1);
+        }
+    }
+}
---
+++ Method Class Assignment/Method Class Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation oper = new Operation();
+
+            oper.Multi(10, 20);
+            oper.Multi(mult1: 10, mult: 4);
+            Console.Read();
+
+        }
+    }
+}

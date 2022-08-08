02ccd592ba2346fe5cb7c0d13e53ed1ce6dccbb2
Tevante Swanston <tevswany@gmail.com>
2022-08-08 11:33:56 -0700

    Operators Assignment

---
+++ Operators/Operators/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public static bool operator ==(Employee employee, Employee employee1)
+        {
+            return employee.Id == employee1.Id;
+
+        }
+
+        public static bool operator !=(Employee employee, Employee employee1)
+        {
+            return employee.Id != employee1.Id;
+        }
+    }
+}
---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Tevante";
+            employee.LastName = "Testing";
+            employee.Id = 2;
+
+            Employee employee2 = new Employee() { FirstName = "Billy", LastName = "Sobu", Id = 8 };
+
+            Console.WriteLine(employee == employee2);
+            Console.ReadLine();
+
+        }
+    }
+}

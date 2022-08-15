a68f661ea16292a76a249eae56528e7cadd63a7d
Tevante Swanston <tevswany@gmail.com>
2022-08-15 14:12:20 -0700

    Final Assignment Submission

---
+++ FinalChallenge/FinalChallenge/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace FinalChallenge
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            using (var ctx = new StudentContext())
+            {
+                var student = new Student() { FirstName = "Tevante", LastName = "Swanston" };
+
+                ctx.Students.Add(student);
+                ctx.SaveChanges();
+            }
+        }
+    }
+
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+
+    public class StudentContext : DbContext
+    {
+        public StudentContext() : base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
